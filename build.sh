5e73c17576cc20de8efa7fb7b9ce08f530dd52f6
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2017-03-03 22:49:25 +0100

    added CNAME

--- build.sh
+++ build.sh
@@ -8,3 +8,6 @@ daux --source=src-3.0 --destination=docs/docs/3.0
 
 mkdir -p ./docs/docs/4.0
 daux --source=src-4.0 --destination=docs/docs/4.0
+
+touch docs/CNAME
+echo spork.bytewelder.com > docs/CNAME
